4b1c181ac44862f23f6e11802511918c1a578f57
sridaopa3 <sridaopa@uoc.edu>
2026-04-08 17:11:20 +0200

    Producto 4

---
+++ src/InnerJoinConElCafe/modelo/Articulo.java
@@ -0,0 +1,68 @@
+package InnerJoinConElCafe.modelo;
+
+public class Articulo {
+
+    private int codigo;
+    private String descripcion;
+    private double precioVenta;
+    private double gastosEnvio;
+    private int tiempoPreparacion;
+
+    public Articulo(String descripcion, double precioVenta, double gastosEnvio, int tiempoPreparacion) {
+        this.descripcion = descripcion;
+        this.precioVenta = precioVenta;
+        this.gastosEnvio = gastosEnvio;
+        this.tiempoPreparacion = tiempoPreparacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Articulo{" +
+                "codigo='" + codigo + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", precioVenta=" + String.format("%.2f", precioVenta) +
+                ", gastosEnvio=" + String.format("%.2f", gastosEnvio) +
+                ", tiempoPreparacion=" + tiempoPreparacion +
+                '}';
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public double getGastosEnvio() {
+        return gastosEnvio;
+    }
+
+    public void setGastosEnvio(double gastosEnvio) {
+        this.gastosEnvio = gastosEnvio;
+    }
+
+    public int getTiempoPreparacion() {
+        return tiempoPreparacion;
+    }
+
+    public void setTiempoPreparacion(int tiempoPreparacion) {
+        this.tiempoPreparacion = tiempoPreparacion;
+    }
+}
---
+++ src/InnerJoinConElCafe/modelo/Cliente.java
@@ -0,0 +1,69 @@
+package InnerJoinConElCafe.modelo;
+
+public abstract class Cliente {
+
+    protected String nombre;
+    protected String domicilio;
+    protected String nif;
+    protected String email;
+
+    public Cliente(String nombre, String domicilio, String nif, String email) {
+        this.nombre = nombre;
+        this.domicilio = domicilio;
+        this.nif = nif;
+        this.email = email;
+    }
+        
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nombre='" + nombre + '\'' +
+                ", domicilio='" + domicilio + '\'' +
+                ", nif='" + nif + '\'' +
+                ", email='" + email + '\'' +
+                "} ";
+    }
+  
+
+
+    //getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double aplicarDescuento(double precioBase) {
+        return precioBase; // El cliente estándar devuelve el precio tal cual
+    }
+
+    public abstract double getCuotaAnual();
+    public abstract double getDescuentoEnvio();
+
+}
